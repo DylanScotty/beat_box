eac991ee6426255bfe1daff60231179eac9a91ed
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-12 15:15:50 -0600

    Add append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,22 @@
 class LinkedList
     attr_reader :head
+    attr_accessor :head
     def initialize
         @head = nil
     end
+
+    def append(data)
+        new_node = Node.new(data)
+        if @head.nil?
+            @head = new_node
+        else
+            current_node = @head
+            until current_node.next_node.nil?
+            end
+            current_node.next_node = new_node
+        end
+        data
+    end
+   
 end
+
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -3,10 +3,22 @@ require "./lib/node"
 
 RSpec.describe Node do
 
-    it 'has data comes back "plop"' do
+    it 'has head equal to nil' do
         list = LinkedList.new
         expect(list.head).to eq(nil)
     end
+
+    it 'will return "doop"' do
+        list = LinkedList.new
+        expect(list.append("doop")).to eq("doop")
+    end
+
+    it 'next node should have nil"' do
+        list = LinkedList.new
+        list.append("doop")
+        expect(list.head.next_node).to eq(nil)
+    end
+
 
 
 
