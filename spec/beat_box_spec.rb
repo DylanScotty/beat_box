9670e862b19a99fbff4ff839723d0df9724ef6e4
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-16 12:09:54 -0600

    Add test question if bb is an instance of BeatBox

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -1,3 +1,10 @@
 require "./lib/beat_box"
 require "./lib/linked_list"
 require "./lib/node"
+
+RSpec.describe BeatBox do
+    it 'is an instance of BeatBox' do
+        bb = BeatBox.new
+        expect(bb).to be_a_instance_of(BeatBox)
+    end
+end
